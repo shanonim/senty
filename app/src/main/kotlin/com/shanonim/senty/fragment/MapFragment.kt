23fb631a2e53931120129f365cf35b8f0d9fd435
shanonim <indigo.03sg@gmail.com>
2017-04-17 22:31:08 +0900

    🍎 [update] display current location on googleMap

--- app/src/main/kotlin/com/shanonim/senty/fragment/MapFragment.kt
+++ app/src/main/kotlin/com/shanonim/senty/fragment/MapFragment.kt
@@ -1,18 +1,24 @@
 package com.shanonim.senty.fragment
 
 import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.shanonim.senty.R
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
@@ -24,6 +30,29 @@ import permissions.dispatcher.RuntimePermissions
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var mMap: GoogleMap? = null
+    private val MIN_TIME: Long = 1000
+    private val MIN_DISTANCE: Float = 50f
+    private val REQUEST_PERMISSION: Int = 1000
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location?) {
+            location?.let {
+                mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude)))
+            }
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            // no-op
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+            // no-op
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+            // no-op
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
@@ -40,9 +69,9 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -56,5 +85,31 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 .addToBackStack(null)
                 .commit()
         mapFragment.getMapAsync(this)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION)
+        } else {
+            Toast.makeText(context, "please allow getting location", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION)
+        }
+    }
+
+    private fun getCurrentLocation() {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
+        }
     }
 }
